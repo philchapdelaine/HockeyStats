d4eefab7e04ca44e2616b2efa66526cc9daf6a27
Philippe Chapdelaine <pchapy@gmail.com>
2022-08-12 10:18:36 -0700

    displays standings

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -1,35 +1,76 @@
-import * as React from 'react';
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-import { useDemoData } from '@mui/x-data-grid-generator';
+import React, { useState, useEffect } from 'react'
+import { DataGrid, } from '@mui/x-data-grid';
+import axios from 'axios';
 
-  const columns = [
-    { field: 'maths', headerName: 'Maths', width: 130 },
-    { field: 'science', headerName: 'Science', width: 130 }
-  ];
-
-  const rows = [
-    { id: 1, maths: 75, science: 60 },
-    { id: 2, maths: 80, science: 70 },
-    { id: 3, maths: 50, science: 80 },
-    { id: 4, maths: 80, science: 60 },
-    { id: 5, maths: 100, science: 90 },
-    ];
+const baseURL = "https://statsapi.web.nhl.com/api/v1";
 
 
+const columns = [
+    { field: 'name', headerName: 'Name', width: 260 },
+    { field: 'wins', headerName: 'Wins', width: 130 },
+    { field: 'losses', headerName: 'Losses', width: 130 },
+];
+
+const rows = [
+    { id: 1, name: 'Canadiens', wins: 60, losses: 22 },
+    { id: 2, name: 'Canucks', wins: 50, losses: 32 },
+    { id: 3, name: 'Flames', wins: 55, losses: 27 }
+];
+
+const parseJSON = (data) => {
+    let resArray = []
+    data.records[0].teamRecords.forEach((teamObject, i) => {
+        let teamName = teamObject.team.name;
+        let teamWins = teamObject.leagueRecord.wins
+        let teamLosses = teamObject.leagueRecord.losses
+        resArray[i] = {
+            id: i,
+            name: teamName,
+            wins: teamWins,
+            losses: teamLosses
+        };
+    })
+    console.log('hello');
+    console.log(resArray);
+    return(resArray);
+};
+
+// axios.get(`https://jsonplaceholder.typicode.com/users`)
+//       .then(res => {
+//         const persons = res.data;
+//         this.setState({ persons });
+//       })
 
 const Standings = () => {
+    const [tableData, setTableData] = useState([])
+    useEffect(() => {
+        axios.get(`${baseURL}/standings?season=20212022`)
+          .then((res) => {
+            const standings = res.data
+            const parsedStandings = parseJSON(standings);
+            setTableData(parsedStandings)
+        })
+      }, [])
+       console.log(tableData)
 
-  return (
-    <div className='standings-container'>
-        <div style={{ height: '80vh', width: '100%' }}>
-        <div style={{ display: 'flex', height: '100%' }}>
-        <div style={{ flexGrow: 1 }}>
-          <DataGrid rows={rows} columns={columns} />
+
+
+    
+    return (
+        <div className='standings-container'>
+            <div style={{ height: '80vh', width: '100%' }}>
+                <div style={{ display: 'flex', height: '100%' }}>
+                    <div style={{ flexGrow: 1 }}>
+                        <DataGrid 
+                            columns={columns}
+                            rows={tableData}
+                            // getRowId={(row) => row.internalId}
+                         />
+                    </div>
+                </div>
+            </div>
         </div>
-        </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Standings
