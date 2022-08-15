94e50e293be6f234fa107f2db975417832785280
Philippe Chapdelaine <pchapy@gmail.com>
2022-08-15 13:47:59 -0700

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../App.css'
+import canadiensLogo from '../resources/canadiens.png'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <img className='canadiens-logo' src={canadiensLogo} alt="logo" />
+    </div>
   )
 }
 
